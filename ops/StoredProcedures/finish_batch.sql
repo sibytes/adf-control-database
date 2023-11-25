d5c6ce83ba69b2e23ce1aa886c9aeecbf7171f07
Shaun Ryan <shaun_chiburi@hotmail.com>
2023-11-25 10:52:11 +0000

    option to fail if boatch not complete

--- ops/StoredProcedures/finish_batch.sql
+++ ops/StoredProcedures/finish_batch.sql
@@ -1,13 +1,19 @@
 create procedure [metadata].[finish_batch]
-  @batch_id int
+  @batch_id int,
+  @raise_error_if_not_complete bit = 0
 as
 begin
+
+  declare @completed_processes int
+  declare @total_processes int
+  declare @msg varchar(500)
 
   declare @succeeded_id int = (
     select [id] 
     from [ops].[status]
     where [status] = 'SUCCEEDED'
   )
+
 
   ;with cte_completed as (
     select
@@ -33,7 +39,9 @@ begin
     [status_id] = iif(
       c.[completed_processes] >= b.[total_processes], 
       @succeeded_id, b.[status_id]
-    )
+    ),
+    @completed_processes = c.[completed_processes],
+    @total_processes = b.[total_processes]
   from [ops].[batch] b
   join [cte_completed] c on b.[id] = c.[batch_id];
 
@@ -48,5 +56,24 @@ begin
   begin
     ;throw 51000, 'pipine has failures', 1;
   end
+
+  if (@completed_processes < @total_processes)
+  begin
+    set @msg = FORMATMESSAGE('Pipline batch_id = %i has completed %i out of %i processes', @batch_id, @completed_processes, @total_processes);
+    if @raise_error_if_not_complete = 1
+    begin
+      ;throw 52000, 'pipline has not completed', 1;
+    end
+
+    select 'WARNING' as [level], @msg as [message]
+
+  end
+  else
+  begin
+  
+    select 'INFO' as [level], @msg as [message]
+
+  end
+
 
 end
